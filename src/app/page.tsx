1ba01f30daaf1d9dedd17b2200a52d7f8f629eb3
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2025-09-06 17:09:01 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { sketch } from "@/app/p5/sketch";
 
 export default function Page() {
   return (
-    <div className="flex flex-col w-screen fixed top-20 font-mono items-center justify-center">
+    <div className="flex flex-col w-screen h-screen fixed top-20 font-mono items-center justify-center">
       <div className="fixed bottom-0 left-0 w-screen h-screen z-[-1]">
         <P5Wrapper sketch={sketch} width={700} height={500} />
       </div>
