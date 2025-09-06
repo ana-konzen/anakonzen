06e0fb5cfed4439f023a98b0c13a64a672d80504
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2025-09-06 18:40:24 -0400

    update

--- src/app/p5/sketch.js
+++ src/app/p5/sketch.js
@@ -2,14 +2,26 @@
 
 let mParticles;
 let step = 0;
+let particleColor;
 
-const maxSteps = 2000;
+const maxSteps = 600;
+
+const palette = ["#73310a", "#80aaed", "#eb4034", "#75160f", "#ffffff"];
+
+const nParticles = 50;
+
+const minHeight = 100;
+
+const noiseScale = 0.005;
+
+let dimension = 1;
 
 export const sketch = {
   setup: (p5) => {
     p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
     // p5.noLoop();
-    mParticles = sketch.createParticles(p5, 200, 0, p5.width + 50, 200, p5.height);
+    mParticles = sketch.createParticles(p5, nParticles, 0, p5.width + 50, minHeight, p5.height);
+    particleColor = palette[0];
 
     p5.stroke(255);
     p5.fill(100, 200, 100);
@@ -18,14 +30,20 @@ export const sketch = {
 
   draw: (p5) => {
     // sketch.drawCircle(p5, p5.width / 2, p5.height / 2, 100);
-    if (step < maxSteps) sketch.drawMountains(p5, "#73310a", 0.4, 0.05);
+    if (step < maxSteps) sketch.drawMountains(p5, particleColor, dimension, noiseScale);
+    if (step === maxSteps) {
+      particleColor = p5.random(palette);
+      dimension = p5.random(100);
+      mParticles = sketch.createParticles(p5, nParticles, 0, p5.width + 50, minHeight, p5.height);
+      step = 0;
+    }
     step++;
   },
 
   windowResized: (p5) => {
     p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
     p5.clear();
-    mParticles = sketch.createParticles(p5, 200, 0, p5.width + 50, 200, p5.height);
+    mParticles = sketch.createParticles(p5, nParticles, 0, p5.width + 50, minHeight, p5.height);
     step = 0;
   },
 
@@ -35,46 +53,45 @@ export const sketch = {
 
   drawMountains: (p5, col, dimension = 2, noiseScale = 0.003) => {
     const strokeColor = p5.color(col);
-
     strokeColor.setAlpha(p5.random(80, 100));
     p5.stroke(strokeColor);
-    p5.strokeWeight(p5.random(0.1, 2.5));
+    p5.strokeWeight(p5.random(0.1, 3));
 
-    sketch.drawFlowField(p5, mParticles, noiseScale, 0, p5.width + 50, 400, p5.height, dimension);
+    sketch.drawFlowField(p5, mParticles, noiseScale, 0, p5.width + 50, minHeight, p5.height, dimension);
   },
 
   drawFlowField: (
     p5,
     particles,
     noiseScale,
-    min_x = 0,
-    max_x = width,
-    min_y = 0,
-    max_y = width,
-    dimension = 1
+    minX = 0,
+    maxX = p5.width,
+    minY = 0,
+    maxY = p5.height,
+    dim = 1
   ) => {
     for (const p of particles) {
       p5.point(p.x, p.y);
 
-      const n = p5.noise(p.x * noiseScale, p.y * noiseScale, dimension);
+      const n = p5.noise(p.x * noiseScale, p.y * noiseScale, dim);
       const a = 2 * p5.PI * n;
       p.x += p5.cos(a);
       p.y += p5.sin(a);
-      if (!sketch.onScreen(p, min_x, max_x, min_y - 50, max_y + 50)) {
-        p.x = p5.random(min_x, max_x);
-        p.y = p5.random(min_y, max_y);
+      if (!sketch.onScreen(p, minX, maxX, minY - 50, maxY + 50)) {
+        p.x = p5.random(minX, maxX);
+        p.y = p5.random(minY, maxY);
       }
     }
   },
 
-  onScreen: (v, min_x, max_x, min_y, max_y) => {
-    return v.x >= min_x && v.x <= max_x && v.y >= min_y && v.y <= max_y;
+  onScreen: (v, minX, maxX, minY, maxY) => {
+    return v.x >= minX && v.x <= maxX && v.y >= minY && v.y <= maxY;
   },
 
-  createParticles: (p5, num, min_x, max_x, min_y, max_y) => {
+  createParticles: (p5, num, minX, maxX, minY, maxY) => {
     const arr = [];
     for (let i = 0; i < num; i++) {
-      arr.push(p5.createVector(p5.random(min_x, max_x), p5.random(min_y, max_y)));
+      arr.push(p5.createVector(p5.random(minX, maxX), p5.random(minY, maxY)));
     }
     return arr;
   },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import { sketch } from "@/app/p5/sketch";
 
 export default function Page() {
   return (
-    <div className="flex flex-col w-screen h-screen fixed top-20 font-mono items-center justify-center">
-      <div className="fixed bottom-0 left-0 w-screen h-screen z-[-1]">
-        <P5Wrapper sketch={sketch} width={700} height={500} />
-      </div>
+    <div className="fixed top-0 left-0 w-screen h-screen z-[-1]">
+      <P5Wrapper sketch={sketch} />
     </div>
   );
 }
