e947242ac771edd93a6665103290b83881b467a7
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2025-09-06 17:51:37 -0400

    update

--- src/app/navMenu.tsx
+++ src/app/navMenu.tsx
@@ -11,13 +11,13 @@ export default function NavMenu() {
   const isActive = (href: string) => pathname === href;
 
   return (
-    <div className="fixed md:text-sm appearance-none font-mono uppercase font-bold top-8 left-3 z-50">
-      <div className="flex appearance-none h-full space-x-8 [writing-mode:vertical-lr]">
+    <div className="fixed md:text-sm font-mono uppercase font-bold top-8 left-3 z-50">
+      <div className="flex flex-col flex-none h-full space-y-8">
         {navLinks.map((link) => (
           <span key={link.href}>
             <Link
               href={link.href}
-              className={`${isActive(link.href) ? "bg-amber-200 [writing-mode:horizontal-tb]" : "hover:bg-amber-200"} appearance-none`}
+              className={`${isActive(link.href) ? "bg-amber-200 [writing-mode:horizontal-tb]" : "hover:bg-amber-200 [writing-mode:vertical-lr]"}`}
             >
               {link.title}
             </Link>
